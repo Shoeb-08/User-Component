8aaf5adf52fb9f793c544589aa8ab733faa83f29
Syed Shoeb Ullah Hussainy <61550309+Shoeb-08@users.noreply.github.com>
2022-08-01 20:59:04 +0530

    Delete patterns directory

--- patterns/Builder pattern/src/builder/webGet.java
+++
@@ -1,9 +0,0 @@
-package builder;
-
-public interface webGet {
-        public void buildBasement();
-        public void buildStructure();
-        public void buildRoof();
-        public void buildInterior();
-        public Ecom getHouse();
-}
--- patterns/DecoratorPattern/src/DecoratorPattern/SpecialCheeseBurger.java
+++
@@ -1,8 +0,0 @@
-package DecoratorPattern;
-
-public class SpecialCheeseBurger extends BaseBurger{
-    public SpecialCheeseBurger(){
-        this.price=60;
-        this.description="Special Burger ";
-    }
-}
